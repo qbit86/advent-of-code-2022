b4970d48d75fe798fd092c21847d9c16c0f538fd
Viktor Ptitselov <qbit86@gmail.com>
2022-11-23 01:46:54 +0200

    Add PuzzleTests

---
+++ tests/PuzzleTests/Tests.cs
@@ -0,0 +1,24 @@
+using System.Text;
+
+namespace AdventOfCode2022;
+
+public sealed class Tests
+{
+    [Theory]
+    [InlineData("sample-input.txt", 0L)]
+    internal async Task PartOne(string inputPath, long expected)
+    {
+        using StreamReader input = new(inputPath, Encoding.UTF8);
+        var actual = await Puzzles.PartOne.SolveAsync(input).ConfigureAwait(false);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory(Skip = "Not implemented yet")]
+    [InlineData("sample-input.txt", 0L)]
+    internal async Task PartTwo(string inputPath, long expected)
+    {
+        using StreamReader input = new(inputPath, Encoding.UTF8);
+        var actual = await Puzzles.PartTwo.SolveAsync(input).ConfigureAwait(false);
+        Assert.Equal(expected, actual);
+    }
+}
