6db17cc1d1f7a5a0ef912c97a3ea447310148354
Viktor Ptitselov <qbit86@gmail.com>
2022-11-22 02:39:11 +0200

    Add Puzzle project

---
+++ src/Puzzle/IPuzzle.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022;
+
+public interface IPuzzle<TResult>
+{
+    Task<TResult> SolveAsync(TextReader input) => Task.FromResult(Solve(input));
+
+    TResult Solve(TextReader input) => SolveAsync(input).Result;
+}
