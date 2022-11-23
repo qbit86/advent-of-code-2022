337beedd02b1e22a5172458ce0b90a77b916a8c1
Viktor Ptitselov <qbit86@gmail.com>
2022-11-23 02:03:14 +0200

    Add ConsoleRunner

---
+++ src/ConsoleRunner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using AdventOfCode2022;
+
+try
+{
+    using StreamReader input = new("input.txt", Encoding.UTF8);
+    long result = await Puzzles.PartOne.SolveAsync(input).ConfigureAwait(false);
+    Console.WriteLine(result);
+}
+catch (NotImplementedException)
+{
+    Console.Error.WriteLine(nameof(NotImplementedException));
+}
+
+try
+{
+    using StreamReader input = new("input.txt", Encoding.UTF8);
+    long result = await Puzzles.PartTwo.SolveAsync(input).ConfigureAwait(false);
+    Console.WriteLine(result);
+}
+catch (NotImplementedException)
+{
+    Console.Error.WriteLine(nameof(NotImplementedException));
+}
