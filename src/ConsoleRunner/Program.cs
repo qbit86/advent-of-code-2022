7dd14ade5b2b41597749123ff4bf79678e906f70
Viktor Ptitselov <qbit86@gmail.com>
2022-12-27 23:31:43 +0200

    Add stopwatch

--- src/ConsoleRunner/Program.cs
+++ src/ConsoleRunner/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using AdventOfCode2022;
 
 using StreamReader input = new("input.txt", Encoding.UTF8);
+var stopwatch = Stopwatch.StartNew();
 long result = await Puzzles.PartOne.SolveAsync(input).ConfigureAwait(false);
+Console.WriteLine(stopwatch.Elapsed);
 Console.WriteLine(result);
